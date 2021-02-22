1c3e2e8bd0c4234974563babd32e41df04a00524
menachem margolis <margolismendel5728@gmail.com>
2021-02-22 14:28:05 -0500

    Done.

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,15 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return(
+    <div id ="about">
+     <h2>About Me</h2>
+     <p>hello montreal, is te greatest city on planet earth </p>
+     <img src ={image} alt ="i made this"></img>
+    </div>
+  )
 }
 
 export default About;
+
+
